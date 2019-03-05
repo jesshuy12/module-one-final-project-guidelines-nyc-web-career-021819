ff33b572fe9a4397f9c0af95bc9ccb4d9fa2c2c3
Jessy Hu <jesshuy12@gmail.com>
2019-03-05 13:23:54 -0500

    update cli 2

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -2,3 +2,11 @@ class User < ActiveRecord::Base
    has_many :job_searches
    has_many :jobs, through: :job_searches
 end
+
+def self.create_username(user_input)
+  self.create(name: user_input)
+end
+
+def self.find_username(user_input)
+  self.find_by(name: user_input)
+end
